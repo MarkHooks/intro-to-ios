eb08041d53901c49ab6da227cea6890789ece16c
Mark <mark@iMac2.local>
2019-02-04 08:33:54 -0500

    finnished the constants worksheet

---
+++ Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+let friends = 899
+print(friends)
+
+/*:
+ Now assume you go through and remove friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is.
+ */
+
+/*:
+ Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
+ */
+print("no, because you cant edit the constant")
+
+//: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
