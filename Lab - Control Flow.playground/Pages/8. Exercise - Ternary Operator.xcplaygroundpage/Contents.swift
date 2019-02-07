2f6de413f3a7e02decafd5d8104b10a46b13c9b8
Mark <mark@iMac2.local>
2019-02-07 12:17:50 -0500

    finnished lab

---
+++ Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+let number1 = 14
+let number2 = 25
+
+var largest: Int = number1 > number2 ? number1 : number2
+
+
+//: [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
